80e0c0fdff14d0224196a7934c2b8a5482900df3
WillMcIntosh <mcintoshwilliamh@gmail.com>
2017-12-06 15:51:21 -0500

    had to follow along with shiffman video to get everything working.

--- fractals/sketch.js
+++ fractals/sketch.js
@@ -1,42 +1,42 @@
+var len = 100;
+var angle = 0;
+
+var slider;
+
 function setup(){
     // put setup code here
     createCanvas(480, 480);
-    background (0);
-    stroke(255);
+    slider = createSlider(0, PI/2, PI/4, 0.1);
 }
 
-var len = 100;
 
 function draw(){
     // put drawing code here
+    background(0);
+    angle = slider.value();
+    stroke(255);
     // translate the origin to bottom center
-    translate(240, 480);
-    // draw the trunk
-    line(0, 0, 0, -len);
+    translate(240, height);
 
-    fractal(len);
+    branch(100);
+
 }
 
-function fractal(len){
-    console.log(len);
-    // move origin and draw a branch
+function branch(len){
+    line(0, 0, 0, -len);
     translate(0, -len);
+    if (len > 4){
+        push();
+        rotate(angle);
+        branch(len*0.67);
+        pop();
 
-    // decrease len size
-    len = len * 0.67
-    
-    // save this origin location
-    push();
-    rotate(PI/4);
-    line(0, 0, 0, -len);
-    // go back to origin and make the
-    // left hand line
-    pop();
-    rotate(-PI/4);
-    line(0, 0, 0, -len);
+        push();
+        rotate(-angle);
+        branch(len*0.67);
+        pop();
 
-    if (len > 40){
-        fractal(len);
     }
-
 }
+
+
